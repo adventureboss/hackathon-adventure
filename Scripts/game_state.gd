3c42ca2ffb810f011977abcd953603d34db6a071
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-02-28 13:38:52 -0700

    Adds dialogue-manager plugin and start sketching the game state

---
+++ Scripts/game_state.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var _dialog_state = {}
+var _items = []
+
+func add_item(item):
+	self._items.append(item)
+
+func set_dialog_state(actor: String, variable: String, value):
+	if not self._dialog_state.has(actor):
+		self._dialog_state[actor] = {}
+		
+	self._dialog_state[actor][variable] = value
+
+func get_dialog_state(actor: String, variable):
+	if not self._dialog_state.has(actor):
+		self._dialog_state[actor] = {}
+	
+	return self._dialog_state[actor][variable]
