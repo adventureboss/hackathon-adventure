5948269ac85a5bda803468cff7e0cca44a374266
Alec Cohan <alcohan@redhat.com>
2022-02-28 14:13:44 -0500

    Update Game scene with new changes

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,6 +3,8 @@ extends Control
 
 const InputResponse = preload("res://Scenes/InputResponse.tscn")
 
+var max_scroll_length := 0
+
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
 onready var scroll = $Background/MarginContainer/Rows/GameInfo/ScrollContainer
 onready var scrollbar = scroll.get_v_scrollbar()
@@ -10,10 +12,12 @@ onready var scrollbar = scroll.get_v_scrollbar()
 
 func _ready() -> void:
 	scrollbar.connect("changed", self, "handle_scrollbar_changed")
-
+	max_scroll_length = scrollbar.max_value
 
 func handle_scrollbar_changed():
-	scroll.scroll_vertical = scrollbar.max_value
+	if max_scroll_length != scrollbar.max_value:
+		max_scroll_length = scrollbar.max_value
+		scroll.scroll_vertical = scrollbar.max_value
 
 
 func _on_Input_text_entered(new_text: String) -> void:
