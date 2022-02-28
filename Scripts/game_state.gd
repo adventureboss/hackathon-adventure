f9c1adae8396430dae5d930657da6c3fde2b69ac
Stephen <stephen.adams@redhat.com>
2022-02-28 17:13:20 -0500

    add the dialog manager and initail dialog

---
+++ Scripts/game_state.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+# dialog state #
+
+# Associate Check-In Dialog #
+var has_met_associate = false
+var associate_active_convo = false
+var has_id_name = false
+var checked_in = false
