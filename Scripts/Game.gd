e73a04d242f7ef7fa30a76b5842807ac1ea8cae7
Alec Cohan <alcohan@redhat.com>
2022-03-01 09:27:56 -0500

    Add real working buttons and button functions

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -5,6 +5,7 @@ const InputResponse = preload("res://Scenes/InputResponse.tscn")
 
 var max_scroll_length := 0
 
+onready var user_cli = $Background/MarginContainer/Rows/InputArea/HBoxContainer/Input
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
 onready var scroll = $Background/MarginContainer/Rows/GameInfo/ScrollContainer
 onready var scrollbar = scroll.get_v_scrollbar()
@@ -24,3 +25,27 @@ func _on_Input_text_entered(new_text: String) -> void:
 	var input_response = InputResponse.instance()
 	input_response.set_text(new_text, "This is where a response would go.")
 	history_rows.add_child(input_response)
+
+
+func _on_TalkToButton_pressed() -> void:
+	user_cli.text += "TALK TO "
+
+
+func _on_GiveButton_pressed() -> void:
+	user_cli.text += "GIVE "
+
+
+func _on_UseButton_pressed() -> void:
+	user_cli.text += "USE "
+
+
+func _on_WalkButton_pressed() -> void:
+	user_cli.text += "WALK "
+
+
+func _on_PickUpButton_pressed() -> void:
+	user_cli.text += "PICK UP "
+
+
+func _on_LookAtButton_pressed() -> void:
+	user_cli.text += "LOOK AT "
