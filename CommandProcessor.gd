242557202c6a32e9e4de4787a54d9e7acd7bd63f
Alec Cohan <44471274+aleccohan@users.noreply.github.com>
2022-03-01 13:05:09 -0500

    Added begninings of command parser as well as intro text thing (#15)

---
+++ CommandProcessor.gd
@@ -0,0 +1,71 @@
+extends Node
+
+
+func process_command(input: String):
+	var words = input.split(" ", false)
+	var first_word = words[0].to_lower()
+	var second_word = ""
+	var third_word = ""
+
+	if words.size() > 1:
+		second_word = words[1].to_lower()
+	if words.size() > 2:
+		third_word = words[2].to_lower()
+
+	match first_word:
+		"talk":
+			return talkTo(third_word)
+		"give":
+			return give(second_word)
+		"use":
+			return use(second_word)
+		"walk":
+			return walk(second_word)
+		"pick":
+			return pickUp(third_word)
+		"look":
+			return lookAt(third_word)
+		_:
+			return "Unrecognized command - Please try again."
+
+
+func talkTo (third_word: String) -> String:
+	if third_word == "":
+		return "TALK TO who?"
+
+	return "You TALK TO %s" % third_word
+
+
+func give (second_word: String) -> String:
+	if second_word == "":
+		return "GIVE what?"
+
+	return "You GIVE %s" % second_word
+
+
+func use (second_word: String) -> String:
+	if second_word == "":
+		return "USE what?"
+
+	return "You USE %s" % second_word
+
+
+func walk (second_word: String) -> String:
+	if second_word == "":
+		return "WALK where?"
+
+	return "You WALK %s" % second_word
+
+
+func pickUp (third_word: String) -> String:
+	if third_word == "":
+		return "PICK UP what?"
+
+	return "You PICK UP %s" % third_word
+
+
+func lookAt (third_word: String) -> String:
+	if third_word == "":
+		return "LOOK AT what?"
+
+	return "You LOOK AT %s" % third_word
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,10 +1,13 @@
 extends Control
 
-
+const Response = preload("res://Scenes/Response.tscn")
 const InputResponse = preload("res://Scenes/InputResponse.tscn")
+
+export (int) var max_lines_remembered := 20
 
 var max_scroll_length := 0
 
+onready var command_processor = $CommandProcessor
 onready var user_cli = $Background/MarginContainer/Rows/InputArea/HBoxContainer/Input
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
 onready var scroll = $Background/MarginContainer/Rows/GameInfo/ScrollContainer
@@ -14,6 +17,10 @@ onready var scrollbar = scroll.get_v_scrollbar()
 func _ready() -> void:
 	scrollbar.connect("changed", self, "handle_scrollbar_changed")
 	max_scroll_length = scrollbar.max_value
+	var starting_message = Response.instance()
+	starting_message.text = "You have arrived at Purple Beret Con! A gathering of nerds from all over to learn about the latest and greatest open source achievements. The conference center doors are in front of you to the North. Nerds are piling in to see what your favorite open source company has to show this time. You can tell by the panicked faces on some associates that things aren't going quite as well as expected. Maybe you should ask around? The Lobby is ahead of you to the North."
+	
+	add_response_to_game(starting_message)
 
 func handle_scrollbar_changed():
 	if max_scroll_length != scrollbar.max_value:
@@ -21,10 +28,27 @@ func handle_scrollbar_changed():
 		scroll.scroll_vertical = scrollbar.max_value
 
 
+func delete_history_beyond_limit():
+	if history_rows.get_child_count() > max_lines_remembered:
+		var rows_to_forget = history_rows.get_child_count() - max_lines_remembered
+		for i in range(rows_to_forget):
+			history_rows.get_child(i).queue_free()
+
+
+func add_response_to_game(response: Control):
+	history_rows.add_child(response)
+	delete_history_beyond_limit()
+
+
 func _on_Input_text_entered(new_text: String) -> void:
+	if new_text.empty():
+		return
+
 	var input_response = InputResponse.instance()
-	input_response.set_text(new_text, "This is where a response would go.")
-	history_rows.add_child(input_response)
+	var response = command_processor.process_command(new_text)
+	print("RESPONSE: ", response)
+	input_response.set_text(new_text, response)
+	add_response_to_game(input_response)
 
 
 func _on_TalkToButton_pressed() -> void:
