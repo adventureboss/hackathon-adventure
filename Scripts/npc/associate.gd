998787d2f5a0594f116fb95d1eed3916a12eb798
Stephen <stephen.adams@redhat.com>
2022-03-02 13:30:38 -0500

    refactor associate dialog (#31)
    * refactor associate dialog
    
    * update dialog reference for associate

--- Scripts/npc/associate.gd
+++ Scripts/npc/associate.gd
@@ -3,5 +3,5 @@ extends "res://Scripts/actor.gd"
 const associate_dialogue = preload("res://Dialogs/associate.tres")
 
 func talk_to():
-	game_state.show_dialogue(associate_dialogue, "associate_main")
+	game_state.show_dialogue(associate_dialogue, "main")
 	return null
