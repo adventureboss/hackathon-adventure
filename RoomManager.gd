9328cd934387f0655f30de6d441c4fb134c790b5
Stephen <stephen.adams@redhat.com>
2022-03-02 11:18:06 -0500

    streamline locking mechanism

--- RoomManager.gd
+++ RoomManager.gd
@@ -2,11 +2,11 @@ extends Node
 
 
 func _ready() -> void:
-	$Entrance.connect_exit_unlocked("north", $Lobby)
-	$Lobby.connect_exit_locked("east", $EastEntrance)
-	$Lobby.connect_exit_locked("west", $WestEntrance)
-	$EastEntrance.connect_exit_unlocked("east", $EastHall)
+	$Entrance.connect_exit("north", $Lobby)
+	$Lobby.connect_exit("east", $EastEntrance, true)
+	$Lobby.connect_exit("west", $WestEntrance, true)
+	$EastEntrance.connect_exit("east", $EastHall)
 	# $EastHall.connect_exit_unlocked("south", $SessionRoom)
-	$EastHall.connect_exit_unlocked("north", $VendorHall)
-	$WestEntrance.connect_exit_unlocked("west", $Ballroom)
+	$EastHall.connect_exit("north", $VendorHall)
+	$WestEntrance.connect_exit("west", $Ballroom)
 	
