a9125d0e30ff2d85ae79eca230cfceb68d78fb55
Stephen <stephen.adams@redhat.com>
2022-03-01 21:17:23 -0500

    Event coordinator dialog (#21)
    * add event coordinator dialog
    
    * add remove_item and handle_turnin to game state
    
    * add event_coordinator dialog

--- Scripts/game_state.gd
+++ Scripts/game_state.gd
@@ -9,6 +9,10 @@ func add_item(item):
 func has_item(item):
 	return self._items.has(item)
 
+func remove_item(item):
+	var i = _items.find(item)
+	_items.remove(i)
+	
 func set_dialog_state(actor: String, variable: String, value):
 	if not self._dialog_state.has(actor):
 		self._dialog_state[actor] = {}
@@ -21,3 +25,10 @@ func get_dialog_state(actor: String, variable, default = null):
 		self._dialog_state[actor] = {}
 	
 	return self._dialog_state[actor].get(variable, default)
+
+	
+func handle_quest_turnin(item_value):
+	var item_number = get_dialog_state("eventc", "items")
+	var result = item_number + item_value
+	return result
+
