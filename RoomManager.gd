68e76fe3aaa4f964c270e25abac41e1133c14ba6
Stephen <stephen.adams@redhat.com>
2022-03-02 14:22:54 -0500

    lock the right rooms

--- RoomManager.gd
+++ RoomManager.gd
@@ -3,10 +3,10 @@ extends Node
 
 func _ready() -> void:
 	$Entrance.connect_exit("north", $Lobby)
-	$Lobby.connect_exit("east", $EastEntrance, true)
-	$Lobby.connect_exit("west", $WestEntrance, true)
-	$EastEntrance.connect_exit("east", $EastHall)
-	# $EastHall.connect_exit_unlocked("south", $SessionRoom)
+	$Lobby.connect_exit("east", $EastEntrance)
+	$Lobby.connect_exit("west", $WestEntrance)
+	$EastEntrance.connect_exit("east", $EastHall, true)
+	$EastHall.connect_exit("south", $SessionRoom)
 	$EastHall.connect_exit("north", $VendorHall)
-	$WestEntrance.connect_exit("west", $Ballroom)
+	$WestEntrance.connect_exit("west", $Ballroom, true)
 	
