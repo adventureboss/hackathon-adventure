99f03a01041aa9de30c8e87a3ab7050d0616f09e
Stephen <stephen.adams@redhat.com>
2022-03-03 13:45:43 -0500

    update con name

--- Scripts/items/banners.gd
+++ Scripts/items/banners.gd
@@ -1,7 +1,7 @@
 extends "res://Scripts/actor.gd"
 
 func look_at():
-	return "\"Purple Beret Con! Heart stopping, pulse pounding, technology at it’s best.\" Someone in marketing has been watching Nascar."
+	return "\"Purple Cap Con! Heart stopping, pulse pounding, technology at it’s best.\" Someone in marketing has been watching Nascar."
 
 func pick_up():
 	return "Eh. I’m not out to wreck the place"
