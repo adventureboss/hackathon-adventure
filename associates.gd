ad1eca66e86a8e47a0fcf61f3b8f312805fd5327
Stephen <stephen.adams@redhat.com>
2022-03-03 13:49:55 -0500

    update con name in associates.gd

--- associates.gd
+++ associates.gd
@@ -4,4 +4,4 @@ func talk_to():
 	return "They don't look like they want to chat right now. Maybe someone inside is less busy."
 	
 func look_at():
-	return "They're employees of Purple Beret. Based on all the hand waving they are doing, things don't look to be going well."
+	return "They're employees of Purple Cap. Based on all the hand waving they are doing, things don't look to be going well."
