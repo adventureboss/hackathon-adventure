55c7e30122ad2a789ca1f3aad1fef72d50f94209
Stephen <stephen.adams@redhat.com>
2022-03-03 08:50:17 -0500

    improve some dialog

---
+++ associates.gd
@@ -0,0 +1,7 @@
+extends "res://Scripts/actor.gd"
+
+func talk_to():
+	return "They don't look like they want to chat right now. Maybe someone inside is less busy."
+	
+func look_at():
+	return "They're employees of Purple Beret. Based on all the hand waving they are doing, things don't look to be going well."
