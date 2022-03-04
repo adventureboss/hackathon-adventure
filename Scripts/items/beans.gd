33e3c004ea447365ad35478be7e3c4a5e3d56ebb
Stephen <stephen.adams@redhat.com>
2022-03-03 21:37:00 -0500

    add bean eating logic

--- Scripts/items/beans.gd
+++ Scripts/items/beans.gd
@@ -1,7 +1,10 @@
 extends "res://Scripts/actor.gd"
 
 func look_at():
-	return "What kind of person makes this kind of mess anyway? Not me, that’s for sure"
+	if game_state.has_item("beans"):
+		return "Beans, beans, good for your heart..."
+	else:
+		return "What kind of person makes this kind of mess anyway? Not me, that’s for sure"
 
 func pick_up():
 	if game_state.has_item("beans"):
@@ -9,3 +12,10 @@ func pick_up():
 	
 	game_state.add_item("beans")
 	return "I’ll just take some of the evidence from the crime scene"
+
+func use(with):
+	if with.name == "self":
+		game_state.set_dialog_state("player", "caffeinated", 2)
+		return PoolStringArray(["Here goes nothing","[i]You eat the beans raw[/i]", "Blech! Tastes like sand and fingernail polish.", "Oh look! I can see between atoms."]).join("/n")
+	if with.name == "cat":
+		return "I don't think he'll eat them raw."
