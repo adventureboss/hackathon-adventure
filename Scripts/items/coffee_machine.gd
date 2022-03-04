7ce5eb1e5aa33b4b581da32904a0dee57209b03e
Stephen <stephen.adams@redhat.com>
2022-03-04 12:14:43 -0500

    fix puzzle break bug

--- Scripts/items/coffee_machine.gd
+++ Scripts/items/coffee_machine.gd
@@ -12,14 +12,16 @@ func look_at():
 	return "It’s a fancy " + keyword("coffee machine") + ". It says, “uses the strongest beans known to man.” Looks like it makes tea too! I can even have a " + keyword("stir stick") + "!"
 
 func pick_up():
+	if game_state.get_dialog_state("cat", "complete") == false:
+		return "I'd try, but I could drop it on the " + keyword("cat") + ". He should move first."
 	if game_state.get_dialog_state("coffee_machine", "broken") == true:
 		return "I would, but that's a lot of parts to carry."
-	if game_state.get_dialog_state("cat", "complete") == 0:
-		return "I'd try, but I could drop it on the " + keyword("cat") + ". He should move first."
 	game_state.set_dialog_state("coffee_machine", "broken", true)
 	return "“You’re coming with me, fancy " + keyword("coffee machine") + "!” You attempt to pick it up and drop it with a loud crash. " + keyword("Beans") + " spill out all over the floor. “Whoops”"
 
 func talk_to():
+	if game_state.get_dialog_state("coffee_machine", "broken") == true:
+		return "Hello! Coffee machine! Yep, it's broken."
 	game_state.show_dialogue(dialogue, "main")
 
 func use(with):
