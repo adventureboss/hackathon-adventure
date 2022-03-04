e65bd3eb8c8ddc66b94d7306cd86e36faaa25613
Stephen <stephen.adams@redhat.com>
2022-03-04 17:02:56 -0500

    Update button.gd
    add ability to try to push the button with your finger

--- Scripts/items/button.gd
+++ Scripts/items/button.gd
@@ -4,5 +4,8 @@ func look_at():
 	return "It's a small hole with a reset " + keyword("button") + ".  If only I could push it"
 
 func use(with):
-	return with.use(self)
+	if with.name == "self":
+		return "I can't push it. My fingers are too big"
+	else:
+		return with.use(self)
 
