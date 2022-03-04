c45c3d2ac38b0733ee1517ef3addb83297ff6a3e
Stephen <stephen.adams@redhat.com>
2022-03-03 21:27:26 -0500

    add description to pamphlets

--- Scripts/items/pamphlets.gd
+++ Scripts/items/pamphlets.gd
@@ -2,9 +2,9 @@ extends "res://Scripts/actor.gd"
 
 func look_at():
 	if game_state.has_item("pamphlet"):
-		return "Introducing the new [something]"
+		return "Introducing the new Retro Action Multi Dynamic Capacitation Encabulator. Neat."
 		
-	return "There is a pamphlet on every chair"
+	return "There is a " + keyword("pamphlet") + " on every chair"
 	
 func pick_up():
 	game_state.add_item("pamphlet")
