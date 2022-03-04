ab40a28eee87625a8df236fda61e66eec0bebccf
Stephen <stephen.adams@redhat.com>
2022-03-04 16:45:45 -0500

    update t-shirt look

--- Scripts/t-shirt.gd
+++ Scripts/t-shirt.gd
@@ -2,7 +2,7 @@ extends "res://Scripts/actor.gd"
 
 
 func look_at():
-	return "I went to Purple Beret Con and all I got was this " + keyword("t-shirt") + ""
+	return "I went to Purple Cap Con and all I got was this " + keyword("t-shirt") + ""
 
 func pick_up():
 	return "No thanks. I'll pass"
