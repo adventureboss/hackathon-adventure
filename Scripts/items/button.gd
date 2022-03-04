c523aca3b81fb24da8872f41ed121599815bc6f4
Alec Cohan <alcohan@redhat.com>
2022-03-04 15:53:34 -0500

    fix stir stick puzzle

--- Scripts/items/button.gd
+++ Scripts/items/button.gd
@@ -3,15 +3,6 @@ extends "res://Scripts/actor.gd"
 func look_at():
 	return "It's a small hole with a reset " + keyword("button") + ".  If only I could push it"
 
+func use(with):
+	return with.use(self)
 
-func use(object):
-	if object.name == "self":
-		return "My fingers are too big.  I can't just push it"
-	elif object == "stir_stick":
-		game_state.add_item("password")
-		return PoolStringArray([
-			"Aha! I pressed it. it must be resetting.  It says the " + keyword("password") + " is 'PowerBacon'.  Now I'm hungry.",
-			"<Password added to your inventory>"
-		]).join("\n")
-	else:
-		return "That won't work"
