8e87a2aa38ac206686f22445cb888e23df6ef854
Stephen <stephen.adams@redhat.com>
2022-03-03 21:48:10 -0500

    add keyword to vendor_hall.gd

--- Scripts/items/vendor_hall.gd
+++ Scripts/items/vendor_hall.gd
@@ -1,4 +1,4 @@
 extends "res://Scripts/actor.gd"
 
 func look_at():
-	return "It’s the vendor hall. It’s where they keep the swag"
+	return "It’s the vendor hall. It’s where they keep the " + keyword("swag") + "."
