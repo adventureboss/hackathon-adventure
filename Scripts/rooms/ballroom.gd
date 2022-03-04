3ddff4e52f873d9e49c1ac9d6181f5fb9fd9baea
Stephen <stephen.adams@redhat.com>
2022-03-04 15:50:24 -0500

    add keywords to ballroom description

--- Scripts/rooms/ballroom.gd
+++ Scripts/rooms/ballroom.gd
@@ -4,4 +4,4 @@ func get_room_name():
 	return "Ballroom"
 	
 func get_room_description():
-	return "The gem of the entire conference. There is a large stage, rows and rows of seats, and a person standing near a projector machine pulling at their hair."
+	return "The gem of the entire conference. There is a large " + keyword("stage") + ", rows and rows of " + keyword("seats") + ", and a " + keyword("person") + " standing near a projector machine pulling at their hair."
