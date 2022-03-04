1e3aabf340238512822557e2761cc3d9428d1057
Stephen <stephen.adams@redhat.com>
2022-03-03 21:42:37 -0500

    add drink vouchers use

--- Scripts/items/drink_vouchers.gd
+++ Scripts/items/drink_vouchers.gd
@@ -1,1 +1,8 @@
 extends "res://Scripts/actor.gd"
+
+const dialog =  preload("res://Dialogs/coffee_machine.tres")
+
+func use(with):
+	if game_state.has_item("drink_vouchers"):
+		if with.name == "coffee_machine":
+			game_state.show_dialogue(dialog, "main")
