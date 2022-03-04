7967bb34a07a77f38c0a6f9cca51a6db9baa547a
Stephen <stephen.adams@redhat.com>
2022-03-04 11:43:25 -0500

    caffienate the player when they drink coffee

--- Scripts/items/coffee_cup.gd
+++ Scripts/items/coffee_cup.gd
@@ -1,10 +1,17 @@
 extends "res://Scripts/actor.gd"
 
-const dialogue_cat = preload("res://Dialogs/clowder_cat.tres")
+const dialogue = preload("res://Dialogs/clowder_cat.tres")
 
 func look_at():
 	return "It's a cup full of coffee"
 
 func use(with):
 	if with.name == "cat":
-		return "The cat starts vibrating, it growls with the ferocity of a lion and chases you out of the room! *back to the east hallway"
+		game_state.remove_item("coffee_cup")
+		game_state.add_item("cup")
+		game_state.show_dialogue(dialogue, "give_coffee")
+	if with.name == "self":
+		game_state.remove_item("tea_cup")
+		game_state.add_item("cup")
+		game_state.set_dialog_state("player", "caffeinated", 1)
+		return "Mmmm... that was delicious"
