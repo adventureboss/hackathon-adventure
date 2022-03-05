1df2b73fd62609c353ac213ad5a50363a6d0b0be
Stephen <stephen.adams@redhat.com>
2022-03-04 22:56:57 -0500

    make it so we can give items to complete game

--- Scripts/items/lens.gd
+++ Scripts/items/lens.gd
@@ -1,1 +1,7 @@
 extends "res://Scripts/actor.gd"
+
+const dialog = preload("res://Dialogs/event_coordinator.tres")
+
+func give(who):
+	if who.name == "person":
+		game_state.show_dialogue(dialog, "give_lens")
--- Scripts/items/password.gd
+++ Scripts/items/password.gd
@@ -1,6 +1,10 @@
 extends "res://Scripts/actor.gd"
 
+const dialog = preload("res://Dialogs/event_coordinator.tres")
+
 func look_at():
 	return "The password is 'PowerBacon'.  Now I'm hungry"
 
-
+func give(who):
+	if who.name == "person":
+		game_state.show_dialogue(dialog, "give_password")
